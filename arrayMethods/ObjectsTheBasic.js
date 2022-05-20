16c0e637bbe6c321352f51b7c7086b1b7b1b0469
racheleckenrod <rachel@racheleckenrod.com>
2022-05-20 15:12:30 -0500

    chainable

--- arrayMethods/ObjectsTheBasic.js
+++ arrayMethods/ObjectsTheBasic.js
@@ -166,33 +166,39 @@
 // Open a sandbox with tests.
 
 // solution
+
+
 // Chaining
 // importance: 2
 // There’s a ladder object that allows to go up and down:
 
-// let ladder = {
-//   step: 0,
-//   up() {
-//     this.step++;
-//   },
-//   down() {
-//     this.step--;
-//   },
-//   showStep: function() { // shows the current step
-//     alert( this.step );
-//   }
-// };
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this
+  },
+  down() {
+    this.step--;
+    return this
+  },
+  showStep: function() { // shows the current step
+    alert( this.step );
+    return this
+  }
+
+};
 // Now, if we need to make several calls in sequence, can do it like this:
 
-// ladder.up();
-// ladder.up();
-// ladder.down();
-// ladder.showStep(); // 1
-// ladder.down();
-// ladder.showStep(); // 0
+ladder.up();
+ladder.up();
+ladder.down();
+ladder.showStep(); // 1
+ladder.down();
+ladder.showStep(); // 0
 // Modify the code of up, down and showStep to make the calls chainable, like this:
 
-// ladder.up().up().down().showStep().down().showStep(); // shows 1 then 0
+ladder.up().up().down().showStep().down().showStep(); // shows 1 then 0
 // Such approach is widely used across JavaScript libraries.
 
 // Open a sandbox with tests.
